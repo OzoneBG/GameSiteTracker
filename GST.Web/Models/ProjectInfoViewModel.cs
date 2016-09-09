94d6a1cbcdff0ff0573b39b396b07f761f6bc76c
OzoneBG <muzunov875@gmail.com>
2016-09-09 15:33:09 +0300

    ViewComponent for project stats Removed register link Cached output of project stats for 15 minutes

---
+++ GST.Web/Models/ProjectInfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace GST.Web.Models
+{
+    public class ProjectInfoViewModel
+    {
+        public int TotalDownloads { get; set; }
+
+        public string BuildVersion { get; set; }
+
+        public int DaysTillArrival { get; set; }
+
+        public int TotalTasksForBuild { get; set; }
+
+        public int TotalFinishedTasksForBuild { get; set; }
+
+    }
+}
---
+++ GST.Web/ViewComponents/ProjectInfoViewComponent.cs
@@ -0,0 +1,24 @@
+﻿namespace GST.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+
+    public class ProjectInfoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ProjectInfoViewModel tempModel = new ProjectInfoViewModel
+            {
+                TotalDownloads = 23,
+                BuildVersion = "1039b",
+                DaysTillArrival = 12,
+                TotalTasksForBuild = 10,
+                TotalFinishedTasksForBuild = 5
+            };
+
+            tempModel.BuildVersion = 'v' + tempModel.BuildVersion;
+
+            return View(tempModel);
+        }
+    }
+}
