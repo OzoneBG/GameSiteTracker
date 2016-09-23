6e172a79685ce85e823bb10056e4b963935b7856
OzoneBG <muzunov875@gmail.com>
2016-09-23 13:10:59 +0300

    Moved page navigation to viewcomponent to not repeat code in views and reuse it where possible

---
+++ Web/GST.Web/ViewComponents/PageNavigationViewComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace GST.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PageNavigationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int currentPage, int totalLinksToDisplay, string redirectActionName)
+        {
+            ViewData["currentPage"] = currentPage;
+            ViewData["totalLinksToDisplay"] = totalLinksToDisplay;
+            ViewData["redirectActionName"] = redirectActionName;
+
+            return View();
+        }
+    }
+}
