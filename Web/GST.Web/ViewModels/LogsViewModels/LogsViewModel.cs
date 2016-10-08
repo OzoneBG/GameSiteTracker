1190ae4b65adcb95d1c7bf43bd753dedc714d91e
OzoneBG <muzunov875@gmail.com>
2016-10-08 18:12:41 +0300

    Updates to the UI

--- Web/GST.Web/ViewModels/LogsViewModels/LogsViewModel.cs
+++ Web/GST.Web/ViewModels/LogsViewModels/LogsViewModel.cs
@@ -6,6 +6,8 @@
 
     public class LogsViewModel : IMapFrom<Log>
     {
+        public int Id { get; set; }
+
         public string Category { get; set; }
 
         public string Content { get; set; }
